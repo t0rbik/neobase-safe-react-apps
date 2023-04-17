36581ecfd29479ff480c843e126c7b80eccd9f3a
torbik <liker1011@gmail.com>
2023-04-17 17:54:13 +0700

    fix: x

--- apps/wallet-connect/src/constants.ts
+++ apps/wallet-connect/src/constants.ts
@@ -1,6 +1,7 @@
-const { REACT_APP_WALLETCONNECT_PROJECT_ID, NODE_ENV } = process.env
+const { REACT_APP_WALLETCONNECT_PROJECT_ID } = process.env
 
-export const isProduction = NODE_ENV === 'production'
+export const isProduction = false
+// export const isProduction = NODE_ENV === 'production'
 
 export const WALLETCONNECT_V2_PROJECT_ID = REACT_APP_WALLETCONNECT_PROJECT_ID
 
